c1c0395899e988701a87adb9ed357abaec034767
k21091 <k21091kk@aitech.ac.jp>
2024-05-06 18:11:56 +0900

    add:ユーザ登録画面の実装

--- src/src/app/signup/page.tsx
+++ src/src/app/signup/page.tsx
@@ -11,9 +11,11 @@ import {
   FormErrorMessage,
   FormLabel,
   Heading,
+  HStack,
   Input,
   InputGroup,
   InputRightElement,
+  Select,
   useToast,
   VStack,
 } from '@/common/design'
@@ -23,13 +25,25 @@ type formInputs = {
   email: string
   password: string
   confirm: string
+  local: string
+  gender: string
+  day:string
+  given_name:string
+  family_name:string
 }
 
+const customContainerStyle = {
+  borderWidth: "2px", // 枠の太さ
+  borderColor: "gray.200", // 枠の色
+  borderRadius: "md", // 角の丸み
+  padding: "20px", // 内部の余白
+};
 /** サインアップ画面
  * @screenname SignUpScreen
  * @description ユーザの新規登録を行う画面
  */
 export default function SignUpScreen() {
+  const [step, setStep] = useState(1);
   const router = useRouter()
   const toast = useToast()
   const {
@@ -37,22 +51,116 @@ export default function SignUpScreen() {
     register,
     getValues,
     formState: { errors, isSubmitting },
+    setError
   } = useForm<formInputs>()
 
   const [password, setPassword] = useState(false)
   const [confirm, setConfirm] = useState(false)
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
 
-  const onSubmit = handleSubmit(async (data) => {
-  })
+  const onSubmit = handleSubmit(async(data) => {
+    if (step === 1) {
+      try {
+        const formData = new URLSearchParams()
+        formData.append('email', data.email)
+  
+        const response = await fetch('http://localhost:8084/user/check', {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: formData.toString(), // URLエンコードされた文字列をボディとして送信
+        })
+  
+        if (response.ok) {
+          setStep(2);
+        } else {
+          setError('email', {
+            type: 'manual',
+            message: 'このアドレスはすでに使用されています',
+          });
+        }
+      } catch (error) {
+        toast({
+          title: 'エラー',
+          description: 'ネットワークエラーが発生しました。',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+    } else {
+      // 最終データを送信
+      const birthday = new Date(`${year}-${month}-${parseInt(data.day)+1}`);
+      const fullname = data.family_name+" "+data.given_name;
+      //const lang = window.navigator.languages || window.navigator.language;
+      try {
+        const requestBody = {
+          email: data.email, 
+          password: data.password,
+          name_ja: fullname,
+          given_name: data.given_name,
+          family_name:data.family_name,
+          locale:data.local,
+          gender:parseInt(data.gender),
+          birthday:birthday,
+        };
+  
+        const response = await fetch('http://localhost:8084/user/create', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json', // 送信データのタイプ
+        },
+        body: JSON.stringify(requestBody), // JSON形式でデータを送信
+      });
+  
+        if (response.ok) {
+          toast({
+            title: 'ログイン成功',
+            description: 'ログインが成功しました。',
+            status: 'success',
+            duration: 5000,
+            isClosable: true,
+          })
+          const data = await response.json() // JSONレスポンスを取得
+          const redirectUrl = "/signin" // リダイレクト先のURLを取得
+          router.push(redirectUrl) // ログイン成功後のリダイレクト
+        } else {
+          const errorData = await response.json()
+          toast({
+            title: 'ログイン失敗',
+            description: errorData.message || 'ログインに失敗しました。',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          })
+        }
+      } catch (error) {
+        toast({
+          title: 'エラー',
+          description: 'ネットワークエラーが発生しました。',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+    }
+  });
 
+  const backState = () => {
+    setStep(step-1)
+  };
   const passwordClick = () => setPassword(!password)
   const confirmClick = () => setConfirm(!confirm)
 
   return (
     <Flex height='100vh' justifyContent='center' alignItems='center'>
-      <VStack spacing='5'>
+      <VStack spacing='5'　width="350px"　style={customContainerStyle}>
         <Heading>新規登録</Heading>
         <form onSubmit={onSubmit}>
+        {step === 1 && (
           <VStack alignItems='left'>
             <FormControl isInvalid={Boolean(errors.email)}>
               <FormLabel htmlFor='email' textAlign='start'>
@@ -161,22 +269,162 @@ export default function SignUpScreen() {
                 boxShadow: '0 7px 10px rgba(0, 0, 0, 0.3)',
               }}
             >
-              新規登録
+              次へ
+            </Button>
+            <Button
+              as={NextLink}
+              href='/signin'
+              bg='white'
+              width='100%'
+              _hover={{
+                borderColor: 'transparent',
+                boxShadow: '0 7px 10px rgba(0, 0, 0, 0.3)',
+              }}
+            >
+              ログインはこちらから
             </Button>
           </VStack>
+        )}
+        {step === 2 && (
+          <VStack alignItems="left">
+            <HStack spacing="2">
+            <FormControl isInvalid={Boolean(errors.gender)}>
+              <FormLabel>性別</FormLabel>
+              <Select {...register('gender', { required: '必須項目です' })}>
+                <option value="0">選択してください</option>
+                <option value="1">男性</option>
+                <option value="2">女性</option>
+                <option value="9">その他</option>
+              </Select>
+              <FormErrorMessage>
+                {errors.gender && errors.gender.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={Boolean(errors.local)}>
+              <FormLabel>言語</FormLabel>
+              <Select {...register('local', { required: '必須項目です' })}>
+                <option value="">選択してください</option>
+                <option value="ja">日本語</option>
+                <option value="en">English</option>
+                <option value="9">その他</option>
+              </Select>
+              <FormErrorMessage>
+                {errors.local && errors.local.message}
+              </FormErrorMessage>
+            </FormControl>
+            </HStack>
+          <FormControl isInvalid={Boolean(errors.day)}>
+              <FormLabel>生年月日</FormLabel>
+              <HStack spacing="2"> {/* 横に並べる */}
+                <Select
+                  placeholder="年"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                  isRequired
+                >
+                  {Array.from({ length: 100 }, (_, i) => 2023 - i).map((y) => (
+                    <option key={y} value={y}>
+                      {y}
+                    </option>
+                  ))}
+                </Select>
+
+                <Select
+                  placeholder="月"
+                  value={month}
+                  onChange={(e) => setMonth(e.target.value)}
+                  isRequired
+                >
+                  {Array.from({ length: 12 }, (_, i) => i+1).map((m) => (
+                    <option key={m} value={m}>
+                      {m}
+                    </option>
+                  ))}
+                </Select>
+
+                <Input
+                  type="number"
+                  placeholder="日"
+                  {...register('day', {
+                    required: '必須項目です',
+                    min: 1,
+                    max: 32,
+                  })}
+                />
+              </HStack>
+              <FormErrorMessage>
+                {errors.day && errors.day.message}
+              </FormErrorMessage>
+            </FormControl>
+            <HStack spacing="2">
+            <FormControl isInvalid={Boolean(errors.family_name)}>
+              <FormLabel htmlFor='family_name' textAlign='start'>
+                性
+              </FormLabel>
+              <Input
+                id='family_name'
+                {...register('family_name', {
+                  required: '必須項目です',
+                  maxLength: {
+                    value: 50,
+                    message: '50文字以内で入力してください',
+                  },
+                })}
+              />
+              <FormErrorMessage>
+                {errors.family_name && errors.family_name.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={Boolean(errors.given_name)}>
+              <FormLabel htmlFor='given_name' textAlign='start'>
+                名
+              </FormLabel>
+              <Input
+                id='given_name'
+                {...register('given_name', {
+                  required: '必須項目です',
+                  maxLength: {
+                    value: 50,
+                    message: '50文字以内で入力してください',
+                  },
+                })}
+              />
+              <FormErrorMessage>
+                {errors.given_name && errors.given_name.message}
+              </FormErrorMessage>
+            </FormControl>
+            </HStack>
+            
+            <Button
+                marginTop='4'
+                color='white'
+                bg='gray'
+                onClick={backState}
+                paddingX='auto'
+                _hover={{
+                  borderColor: 'transparent',
+                  boxShadow: '0 7px 10px rgba(0, 0, 0, 0.3)',
+                }}
+              >
+                戻る
+              </Button>
+              <Button
+                color='white'
+                bg='teal.400'
+                isLoading={isSubmitting}
+                type='submit'
+                paddingX='auto'
+                _hover={{
+                  borderColor: 'transparent',
+                  boxShadow: '0 7px 10px rgba(0, 0, 0, 0.3)',
+                }}
+              >
+                登録
+              </Button>
+        </VStack>
+        )}
         </form>
-        <Button
-          as={NextLink}
-          href='/signin'
-          bg='white'
-          width='100%'
-          _hover={{
-            borderColor: 'transparent',
-            boxShadow: '0 7px 10px rgba(0, 0, 0, 0.3)',
-          }}
-        >
-          ログインはこちらから
-        </Button>
+        
       </VStack>
     </Flex>
   )
